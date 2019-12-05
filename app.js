ec3f93e09ea6db12459251a342d839a447455ed6
Lissana <liss.glace471@gmail.com>
2019-12-05 22:28:49 +0900

    ヘッダー情報の設定

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const http = require("http");
 
 let server = http.createServer(
     (request, response) => {
-        response.end("Hello Node.js!")
+        response.setHeader("content-Type","text/html");
+        response.write("<!DOCTYPE html><html lang='ja'>");
+        response.write("<head><title>title</title></head>")
+        response.write("<body><h1>Hello</h1><p>Welcome to Node.js!</p>");
+        response.write("</body><html>");
+        response.end()
     }
 );
 
 server.listen(3000);
+console.log("Server start");
