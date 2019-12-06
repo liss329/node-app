c50549366673a020748c79486f7c828d186a6dcf
Lissana <liss.glace471@gmail.com>
2019-12-06 22:24:26 +0900

    テンプレートエンジンejs読み出しの追加

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require("fs");
+const ejs = require("ejs");
 
+const index_page = fs.readFileSync("./index.ejs", "utf-8");
 let server = http.createServer(getFromClient);
 server.listen(3000);
 console.log("Server start");
@@ -9,12 +11,8 @@ console.log("Server start");
  * サーバオブジェクト作成時処理
  */
 function getFromClient(req, res){
-    fs.readFile("./index.html", "utf-8", 
-        (error, data) => {
-            let content = data.replace(/dummy_title/g, "タイトルです").replace(/dummy_content/g, "コンテンツです");
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(content);
-            res.end();            
-        }
-    );
+    let content = ejs.render(index_page);
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(content);
+    res.end();            
 }
