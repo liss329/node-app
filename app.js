525030e41d68e388800c301662b338854af37a3e
Lissana <liss.glace471@gmail.com>
2019-12-07 19:03:46 +0900

    indexページからotherページへのフォーム送信処理

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
+const qs = require("querystring");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
 const other_page = fs.readFileSync("./other.ejs", "utf-8")
@@ -19,31 +20,12 @@ function getFromClient(req, res){
     let url_parts = url.parse(req.url, true);
     switch(url_parts.pathname){
         case "/":
-            let index_obj_content = "これはIndexページです";
-            let query = url_parts.query;
-
-            if(!query.msg != undefined){
-                index_obj_content += `msg：${ query.msg}`;
-            }
-
-            let index_content = ejs.render(index_page, {
-                title: "Indexページ",
-                content:index_obj_content
-            });
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(index_content);
-            res.end();
+            response_index(req, res, url_parts);
             break;
 
-        case "/other.ejs":
-            let other_content = ejs.render(other_page, {
-                title: "Other",
-                content:"これはOtherページです"
-            });
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(other_content);
-            res.end();
-            break;
+        case "/other":
+            response_other(req, res);
+            break;    
         
         case "/style.css":
             res.writeHead(200, {"Content-Type": "text/css"});
@@ -55,8 +37,68 @@ function getFromClient(req, res){
             res.writeHead(200, {"Content-Type": "text/plain"});
             res.end("no page");
             break;
+    }
+}
 
+/**
+ * indexページのアクセス処理
+ */
+function response_index(req, res, url_parts){
+    let msg = "これはIndexページです";
+    let query = url_parts.query;
+
+    if(!query.msg != undefined){
+        msg += `msg：${ query.msg}`;
     }
 
+    let index_content = ejs.render(index_page, {
+        title: "Indexページ",
+        content:msg
+    });
+
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write(index_content);
+    res.end();
+}
+
+/**
+ * otherページのアクセス処理
+ */
+function response_other(req, res){
+    let msg = "これはOtherページです"
+    
+    // POSTアクセス時の処理
+    if(req.method == "POST"){
+        let body = "";
+        
+        //データ受信イベント処理
+        req.on("data", (data) => {
+            body += data;
+        });
+
+        // データ受信終了イベント処理
+        req.on("end", () => {
+            let post_data = qs.parse(body);
+            msg += ` msg：${post_data.msg}`;
+
+            let other_content = ejs.render(other_page, {
+                title: "Other",
+                content:msg
+            });
             
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(other_content);
+            res.end();
+        });
+    }else{
+        msg = "ページがありません"
+        let other_content = ejs.render(other_page, {
+            title: "Other",
+            content:msg
+        });
+
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(other_content);
+        res.end();
+    }
 }
